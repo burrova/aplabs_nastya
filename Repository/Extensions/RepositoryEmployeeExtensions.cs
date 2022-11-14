0898f009de889f094526040eb6dc1359e47b1f37
burrova <cinemamhenlightened@yandex.ru>
2022-11-14 23:49:26 +0300

    Sort update

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -29,8 +29,26 @@ string orderByQueryString)
         {
             if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return employees.OrderBy(e => e.Name);
-            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
-            if (string.IsNullOrWhiteSpace(orderQuery))
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public |
+            BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi =>
+                pi.Name.Equals(propertyFromQueryName,
+               StringComparison.InvariantCultureIgnoreCase));
+                if (objectProperty == null)
+                    continue;
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+            }
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' '); if
+           (string.IsNullOrWhiteSpace(orderQuery))
                 return employees.OrderBy(e => e.Name);
             return employees.OrderBy(orderQuery);
         }
